94a2b4898837479a095dc36db2a47143e69c9509
Pascingo <pascal.ingold@hispeed.ch>
2026-03-20 13:04:43 +0100

    First Upload as a Test

---
+++ Data_Preparation.R
@@ -0,0 +1,8 @@
+path_listings <- "./Data_Vaud_all/listings-2.csv"
+path_neigborhoods <- "./Data_Vaud_all/neighbourhoods.csv"
+path_reviews_2 <- "./Data_Vaud_all/reviews-2.csv"
+path_calendar <- "./Data_Vaud_all/calendar.csv"
+raw_data_listings <- read.csv(path_listings, header = TRUE)
+raw_data_neigborhoods <- read.csv(path_neigborhoods, header = TRUE)
+raw_data_reviews <- read.csv(path_reviews_2, header = TRUE)
+raw_data_calendar <- read.csv(path_calendar, header = TRUE)
